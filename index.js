b1e40f2cf8e9b205f46df06f23fad3d266939f88
Hamdan Basri <38688313+hamdanbasri@users.noreply.github.com>
2024-09-25 01:09:01 +0800

    Added Opinions

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function Unity(){
+    document.getElementById('opinion').innerText = 
+    "I started out with Unity, it is an easy to understand game engine that has a huge community support.";
+}
+
+function Unreal(){
+    document.getElementById('opinion').innerText = "Unreal is a great tool to make games and simulation, it has a blueprint editor that makes it easier for designers to help out with the logic.";
+}
+
+function AI(){
+    document.getElementById('opinion').innerText = "I am very new to AI, but in my opinion it is a great tool to generate useful contents and information.";
+}
